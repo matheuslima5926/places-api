9b339629b3e9467d825bba2509d4f75199c8d742
matheus <matheuslima5926@gmail.com>
2021-04-08 00:06:55 -0300

    include tests for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  validates_presence_of :email, :password, :password_confirmation
+  validates_uniqueness_of :email
 end
